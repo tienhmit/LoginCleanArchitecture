3c064476fc98110a184c3955d606bc1052dffc2b
tienhmit <tienitdnu@gmail.com>
2021-08-12 22:49:32 +0700

    first commit

---
+++ LoginCleanArchitectureTests/LoginCleanArchitectureTests.swift
@@ -0,0 +1,30 @@
+//
+//  LoginCleanArchitectureTests.swift
+//  LoginCleanArchitectureTests
+//
+//  Created by Hoang Manh Tien on 8/11/21.
+//
+
+import XCTest
+
+class LoginCleanArchitectureTests: XCTestCase {
+
+//    let userCase = LoginUserCase()
+    override func setUpWithError() throws {
+        
+    }
+    
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
